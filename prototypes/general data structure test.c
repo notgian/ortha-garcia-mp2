d4db8dfb55d8fe539fa8eedab5123efb97e8db2d
Gian Ortha <85060753+notgian@users.noreply.github.com>
2025-02-21 07:25:25 +0800

    added functional bus initialization, with sample main function included, commented out at the bottom

--- prototypes/general data structure test.c
+++ prototypes/general data structure test.c
@@ -18,7 +18,11 @@ struct Bus {
     struct Bus *next;
 };
 
-struct Passenger createEmptyPassenger() 
+/* createEmptyPassenger: returns a Passenger with all values set to an empty value or zero.
+    @return Passenger with empty or zero values   
+*/ 
+struct Passenger 
+createEmptyPassenger() 
 {
     struct Passenger emptyPassenger;
     emptyPassenger.priority = 0;
@@ -28,7 +32,13 @@ struct Passenger createEmptyPassenger()
     return emptyPassenger;
 }
 
-void createEmptyPassengers(struct Passenger *passengerList, int n)
+/* setEmptyPassenger: Takes an array of passengers and sets them all to empty
+   @param *passengerList - array of passengers 
+   @param n - number of passengers to set
+   @return void
+*/ 
+void 
+setEmptyPassengers(struct Passenger *passengerList, int n)
 {
     int i;
     for (i=0; i<n; i++)
@@ -37,40 +47,51 @@ void createEmptyPassengers(struct Passenger *passengerList, int n)
     }
 }
 
-// Accepts an array of pointers to busses 
-void initializeBusses(struct Bus bussesManilaToLaguna[10], struct Bus bussesLagunaToManila[12])
+/* setEmptyPassenger: Takes an array of passengers and sets passenger at index n to empty   
+   @param *passengerList - array of passengers 
+   @param n - index of passenger to set to empty
+   @return void
+*/ 
+void 
+setEmptyPassenger(struct Passenger *passengerList, int n)
 {
-    struct Bus busInst;
+    passengerList[n] = createEmptyPassenger();
+}
 
-    // Initialization of all busses for the Manila to Laguna Trip
-    bussesManilaToLaguna[0] = busInst;
-    bussesManilaToLaguna[1] = busInst;
-    bussesManilaToLaguna[2] = busInst;
-    bussesManilaToLaguna[3] = busInst;
-    bussesManilaToLaguna[4] = busInst;
-    bussesManilaToLaguna[5] = busInst;
-    bussesManilaToLaguna[6] = busInst;
-    bussesManilaToLaguna[7] = busInst;
-    bussesManilaToLaguna[8] = busInst;
-    bussesManilaToLaguna[9] = busInst;
+/* initializeBuses: Takes an two arrays of Bus with size 10 and 12 respectively for Manila and Laguna trips. Initializes the values for all buses. 
+                    NOTE - Contents of arrays do not matter, only size. They contents will be set by this function.
+   @param busesManilaToLaguna - array of buses, size 10  
+   @param busesLagunaToManila - index of buses, size 12
+   @return void
+*/ void initializeBuses(struct Bus busesManilaToLaguna[10], struct Bus busesLagunaToManila[12])
+{
+    struct Bus busInst; // Instantiation of a bus for defining all buses
 
-    // Initialization of busses for the Laguna to Manila Trip
-    bussesLagunaToManila[0] = busInst;
-    bussesLagunaToManila[1] = busInst;
-    bussesLagunaToManila[2] = busInst;
-    bussesLagunaToManila[3] = busInst;
-    bussesLagunaToManila[4] = busInst;
-    bussesLagunaToManila[5] = busInst;
-    bussesLagunaToManila[6] = busInst;
-    bussesLagunaToManila[7] = busInst;
-    bussesLagunaToManila[8] = busInst;
-    bussesLagunaToManila[9] = busInst;
-    bussesLagunaToManila[10] = busInst;
-    bussesLagunaToManila[11] = busInst;
+    // Initialization of all buses for the Manila to Laguna Trip
+    busesManilaToLaguna[0] = busInst;
+    busesManilaToLaguna[1] = busInst;
+    busesManilaToLaguna[2] = busInst;
+    busesManilaToLaguna[3] = busInst;
+    busesManilaToLaguna[4] = busInst;
+    busesManilaToLaguna[5] = busInst;
+    busesManilaToLaguna[6] = busInst;
+    busesManilaToLaguna[7] = busInst;
+    busesManilaToLaguna[8] = busInst;
+    busesManilaToLaguna[9] = busInst;
 
-    //
-    //
-    //
+    // Initialization of buses for the Laguna to Manila Trip
+    busesLagunaToManila[0] = busInst;
+    busesLagunaToManila[1] = busInst;
+    busesLagunaToManila[2] = busInst;
+    busesLagunaToManila[3] = busInst;
+    busesLagunaToManila[4] = busInst;
+    busesLagunaToManila[5] = busInst;
+    busesLagunaToManila[6] = busInst;
+    busesLagunaToManila[7] = busInst;
+    busesLagunaToManila[8] = busInst;
+    busesLagunaToManila[9] = busInst;
+    busesLagunaToManila[10] = busInst;
+    busesLagunaToManila[11] = busInst;
 
     int i;
     int initialTripNo_A = 101;
@@ -79,67 +100,75 @@ void initializeBusses(struct Bus bussesManilaToLaguna[10], struct Bus bussesLagu
     // Setting essential properties for the Manila to Laguna trip
     for (i=0; i<10; i++)
     {
-        createEmptyPassengers(bussesManilaToLaguna[i].passengers, 16);
-        bussesManilaToLaguna[i].tripNumber = initialTripNo_A + i;
-    }
-
-    for (i=9; i>0; i--)
-    {
-        bussesManilaToLaguna[i-2].next = &bussesManilaToLaguna[i];
+        setEmptyPassengers(busesManilaToLaguna[i].passengers, 16);
+        busesManilaToLaguna[i].tripNumber = initialTripNo_A + i;
     }
 
     // Setting essential properties for the Laguna to Manila trip
     for (i=0; i<12; i++)
     {
-        createEmptyPassengers(bussesLagunaToManila[i].passengers, 16);
-        bussesLagunaToManila[i].tripNumber = initialTripNo_B + i;
+        setEmptyPassengers(busesLagunaToManila[i].passengers, 16);
+        busesLagunaToManila[i].tripNumber = initialTripNo_B + i;
     }
 
-    for (i=11; i>0; i--)
+    // Setup linking of buses for Manila to Laguna Trip
+    for (i=9; i>=0; i--)
     {
-        bussesLagunaToManila[i-1].next = &bussesLagunaToManila[i];
+        if (i > 7) // for 109 and 110 (special trip)
+        {
+            struct Bus newBusInst = busInst;
+            busInst.tripNumber = -1;
+            busesManilaToLaguna[i].next = &newBusInst;
+        }
+        else
+        {
+            busesManilaToLaguna[i].next = &busesManilaToLaguna[i+2];
+        }
     }
 
-    /*
+    // Setup linking of buses for Manila to Laguna Trip
+    for (i=11; i>=0; i--)
+    {
+        if (i > 9) // for 160 and 161 (for special trip)
+        {
+            struct Bus newBusInst = busInst;
+            busInst.tripNumber = -1;
+            busesLagunaToManila[i].next = &newBusInst;
+        }
+        else
+        {
+            busesLagunaToManila[i].next = &busesLagunaToManila[i+2];
+        }
+    }
     
-    ===========================================================================================
-    ===========================================================================================
-    ===========================================================================================
-    ===========================================================================================
-    ===========================================================================================
-    
-    */
-    
-    // Returns the root busses in each trip
 }
 
 
-int main() 
-{
+// int main() 
+// {
+//     // Sample Main Function with initialization of buses
+//     struct Bus busesManilaToLaguna[10];
+//     struct Bus busesLagunaToManila[12];
 
-    struct Bus bussesManilaToLaguna[10];
-    struct Bus bussesLagunaToManila[12];
+//     initializeBuses(busesManilaToLaguna, busesLagunaToManila);
 
-    initializeBusses(bussesManilaToLaguna, bussesLagunaToManila);
-
-    int i;
-    struct Bus *currentBus; 
+//     int i; 
     
-    // for (i=0; i<10; i++)
-    // {
-    //     printf("Bus Trip No: %d\n", bussesManilaToLaguna[i].tripNumber);
-    // }
 
-    // for (i=0; i<12; i++)
-    // {
-    //     printf("Bus Trip No: %d\n", bussesLagunaToManila[i].tripNumber);
-    // }
+//     for (i=0; i<10; i++)
+//     {
+//         printf("Current Trip No: %d\n", busesManilaToLaguna[i].tripNumber);
+//         printf("Next Trip No: %d\n", busesManilaToLaguna[i].next->tripNumber);
+//         printf("=================== \n");
+//     }
 
-    for (i=0; i<9; i++)
-    {
-        printf("Current Trip No: %d\n", bussesManilaToLaguna[i].tripNumber);
-        printf("Next Trip No: %d\n", bussesManilaToLaguna[i].next->tripNumber);
-        printf("=================== \n");
-    }
-    return 0;
-}
+//     printf("\n\n");
+
+//     for (i=0; i<12; i++)
+//     {
+//         printf("Current Trip No: %d\n", busesLagunaToManila[i].tripNumber);
+//         printf("Next Trip No: %d\n", busesLagunaToManila[i].next->tripNumber);
+//         printf("=================== \n");
+//     }
+//     return 0;
+// }
